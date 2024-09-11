075a4d2a04b70d7bc614512a2646e0b5704228e0
diegorezm <dmourarezende@gmail.com>
2024-09-11 13:26:24 -0300

    better code

--- d1/main.py
+++ d1/main.py
@@ -4,27 +4,25 @@ from typing import List
 
 import pandas as pd
 
-dir_path = "../Datasets/dados_emprestimos/"
 
-files = [join(dir_path,f) for f in listdir(dir_path) if isfile((join(dir_path, f)))]
+def read_dir(dir_path: str) -> pd.DataFrame:
+    files = [join(dir_path,f) for f in listdir(dir_path) if isfile((join(dir_path, f)))]
+    dtfs: List[pd.DataFrame] = []
+    for file in files:
+        csv = pd.read_csv(file)
+        dtfs.append(csv)
+    embeded = pd.concat(dtfs, ignore_index=True)
+    combined_dtfs = embeded.astype({
+        'id_emprestimo': 'int',
+        'codigo_barras': 'str',
+        'data_renovacao': 'str',
+        'data_emprestimo': 'str',
+        'data_devolucao': 'str',
+        'matricula_ou_siape': 'str',
+        'tipo_vinculo_usuario': 'str'
+    })
+    return combined_dtfs
 
-# id_emprestimo,codigo_barras,data_renovacao,data_emprestimo,data_devolucao,matricula_ou_siape,tipo_vinculo_usuario
-
-dtfs: List[pd.DataFrame] = []
-
-for file in files:
-    csv = pd.read_csv(file)
-    dtfs.append(csv)
-
-embeded = pd.concat(dtfs, ignore_index=True)
-
-combined_dtfs = embeded.astype({
-    'id_emprestimo': 'int',
-    'codigo_barras': 'str',
-    'data_renovacao': 'str',
-    'data_emprestimo': 'str',
-    'data_devolucao': 'str',
-    'matricula_ou_siape': 'str',
-    'tipo_vinculo_usuario': 'str'
-})
-print(combined_dtfs)
+if __name__ == '__main__':
+    path = "../Datasets/dados_emprestimos"
+    print(read_dir(path))
